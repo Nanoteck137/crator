6fc69daac7587677c16366494ae686eccc194df9
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-09 18:49:53 +0100

    Working on recreating the directory structure from template

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/kr/pretty"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	dir := "./work/test"
+	dir = filepath.Clean(dir)
+
+	var dirs []string
+
+	err := filepath.WalkDir(dir, func(p string, d fs.DirEntry, err error) error {
+		fmt.Printf("path: %v\n", p)
+
+		if d.IsDir() && p != dir {
+			fmt.Println(d.Info())
+			dirs = append(dirs, strings.TrimPrefix(p, dir + "/"))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pretty.Println(dirs)
+
+	dst := "./work/dest"
+	for _, dir := range dirs {
+		p := path.Join(dst, dir)
+		err := os.MkdirAll(p, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
