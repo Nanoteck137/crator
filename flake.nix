f69042d11a2062cd80838d162ab0b75ff422cb8e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-11 07:18:56 +0100

    Added nix build

--- flake.nix
+++ flake.nix
@@ -13,8 +13,19 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        crator = pkgs.buildGoModule {
+          pname = "crator";
+          version = self.shortRev or "dirty";
+          src = ./.;
+
+          vendorHash = "sha256-Hng6WZng+64wCvNq3hBQlLAsexAfU+ifFxeUjVDyofk=";
+        };
       in
       {
+        packages.default = crator;
+        packages.crator = crator;
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
